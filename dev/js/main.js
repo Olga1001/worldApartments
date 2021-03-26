fc74b5263c65566592ff3f056d2bf78ec9ee2ad8
Olga <zhuravel.olga95@gmail.com>
2021-03-27 00:45:50 +0200

    in process main page

--- dev/js/main.js
+++ dev/js/main.js
@@ -6,5 +6,25 @@ $(document).ready(function () {
     $(".header-main").toggleClass('shadow');
   });
 
+  // stopPropagation
+  $(".stopPropagation").click(function (e) {
+    e.stopPropagation();
+  });
 
+  // select
+  $(".select-item").click(function () {
+    $(this).toggleClass('active');
+    $(this).siblings(".select-drop").slideToggle(300);
+  });
+  $("html, body").click(function () {
+    $(".select-item").removeClass('active');
+    $(".select-drop").slideUp(300);
+  });
+  $(".select-option").click(function () {
+    let optionText = $(this).text();
+    $(this).addClass('active').siblings().removeClass('active');
+    $(this).closest(".select").find(".select-item span").text(optionText);
+    $(".select-item").removeClass('active');
+    $(".select-drop").slideUp(300);
+  });
 });
